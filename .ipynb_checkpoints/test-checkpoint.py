98c3fe2207483542b78d2e36f92b64c4bf3baf7b
a428327tw_msi <a428327tw@gmail.com>
2024-11-19 12:36:11 +0800

    11/19

---
+++ .ipynb_checkpoints/test-checkpoint.py
@@ -0,0 +1,14 @@
+import torch
+import numpy as np
+
+'''
+print("Hello, World?")
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+'''
+
+from argparse import ArgumentParser
+
+parser1 = ArgumentParser()
+#
+# print(parser1)
